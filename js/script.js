9a89e820296105de51990e072039ed7ad3650778
fbari687 <fbari687@gmail.com>
2023-05-17 16:25:06 +0700

    membuat modal

--- js/script.js
+++ js/script.js
@@ -16,3 +16,32 @@ function changeParentClass(checkbox) {
     parent.classList.remove("active");
   }
 }
+
+// Ambil elemen yang diperlukan
+let modal = document.getElementById("modalSelesai");
+let btn = document.getElementById("selesai");
+let btnBlm = document.getElementsByClassName("belumBtn")[0];
+let btnYa = document.getElementsByClassName("iyaBtn")[0];
+
+// Ketika tombol diklik, buka modal
+btn.addEventListener("click", function () {
+  modal.style.display = "flex";
+  modal.children[0].style.display = "flex";
+});
+
+btnYa.addEventListener("click", function () {
+  modal.children[0].style.display = "none";
+  modal.children[1].style.display = "flex";
+});
+
+btnBlm.addEventListener("click", function () {
+  modal.style.display = "none";
+});
+
+// Jika pengguna mengklik di luar area modal, tutup modal
+window.addEventListener("click", function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+    modal.children[1].style.display = "none";
+  }
+});
